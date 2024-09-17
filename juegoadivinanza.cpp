d4232e15f908ca66382031c1d682985045fdf1a5
tavopaez <gustavo.paez@ucb.edu.bo>
2024-09-17 08:52:20 -0400

    2024-09-17_01 iniciando codigo juego adivinanza

---
+++ juegoadivinanza.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main (){
+
+std::cout<<"############################################################";<<std::endl;
+std::cout<<"BIENVENIDO AL JUEGO ADIVINANZA!";<<std::endl;
+std::cout<<"############################################################";<<std::endl;
+}
+
